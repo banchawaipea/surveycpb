61ff3030dc1c7b8aa30e3b6b2f9c6c78410b760b
BANCHA WAIPEA <banchawaiwai4@gmail.com>
2022-09-15 14:56:08 +0700

    update

--- app/views.py
+++ app/views.py
@@ -8,4 +8,14 @@ def Index(request):
     return render(request, 'main.html')
 
 def Login(request):
+    if request.method == 'POST':
+        data = request.POST.copy()
+        name = data.get('name')
+        price = data.get('price')
+        detail = data.get('detail')
+        imageurl = data.get('imageurl')
     return render(request, 'sign-in.html')
+
+def Signup(request):
+
+    return render(request, 'sign-up.html')
