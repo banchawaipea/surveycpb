7734f407d05b640c9929ff4a0172947b268c9846
BANCHA WAIPEA <banchawaiwai4@gmail.com>
2022-11-24 19:43:11 +0700

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,7 +1,8 @@
 from django.contrib.gis.db import models # import the models gis module
 from django.contrib.auth.models import User #import the user model
 from django.utils import timezone #import the timezone module
-import time
+
+
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -10,6 +11,19 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.username
 
+
+# เก็บข้อมูลหลักเขตโฉนดที่ดิน
+class Point_boundary(models.Model):
+    # id = models.AutoField(primary_key=True)
+    geom = models.PointField(srid=4326, null=True, blank=True)
+    user_add = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    landmark_key = models.CharField(max_length = 100, null=True, blank=True) #หมวดเลขที่หลักเขต
+    landmark_section = models.CharField(max_length = 100, null=True, blank=True) #หมายเลขหลักเขต
+    def __str__(self):
+        return 'หลักเขตโฉนดที่ดินหมายเลข {} - {} เพิ่มข้อมูลโดย {} {}'.format(self.landmark_key, self.landmark_section, self.user_add.user.username, self.user_add.usertype)
+
+
+# เก็บข้อมูลแปลงโฉนดที่ดิน    
 class Asset_deed(models.Model):
     geom = models.PolygonField(srid=4326, null=True, blank=True) #geometry field
     deed_no = models.IntegerField() # โฉนดที่ดินเลขที่
@@ -25,6 +39,7 @@ class Asset_deed(models.Model):
     sqw_number = models.FloatField() #ตารางวา
     date_add = models.DateField(default=timezone.now) #วันที่เพิ่มข้อมูล
     def __str__(self):
-        return self.deed_no, self.land_no
+        return 'โฉนดแปลงหมายเลข {} ที่ดินหมายเลข {}'.format(self.deed_no, self.land_no)
+        # return str(self.deed_no), str(self.land_no)
     
     
